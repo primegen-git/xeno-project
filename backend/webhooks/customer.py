219255439efd58bcc690136700cceb6363b4bec5
ujjawal kumar <ujjawal17032002@gmail.com>
2025-12-04 21:39:45 +0530

    change in customer create router

--- backend/webhooks/customer.py
+++ backend/webhooks/customer.py
@@ -6,6 +6,7 @@ router = APIRouter()
 
 @router.post("/create")
 async def create_customer(req: Request):
+    print(req.headers)
     body = await req.json()
     print(body)
     return {"status", True}
