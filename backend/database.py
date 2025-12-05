17185d1c15db7d81dca2b15c1285368b1c5549b8
ujjawal kumar <ujjawal17032002@gmail.com>
2025-12-05 21:37:32 +0530

    remove sys.exit(1)

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 import os
-import sys
+from fastapi.exceptions import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -11,34 +11,34 @@ DATABASE_USER = os.getenv("POSTGRES_USER", None)
 
 if DATABASE_USER is None:
     print("DATABASE USER IS UNKNOWN")
-    sys.exit(1)
+    raise HTTPException(detail="Database user does not exist", status_code=500)
 
 
 DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD", None)
 
 if DATABASE_PASSWORD is None:
     print("DATABASE PASSWORD IS UNKNOWN")
-    sys.exit(1)
+    raise HTTPException(detail="Database Password does not exist", status_code=500)
 
 DATABASE_HOST = os.getenv("POSTGRES_HOST", None)
 
 if DATABASE_HOST is None:
     print("DATABASE HOST IS UNKNOWN")
-    sys.exit(1)
+    raise HTTPException(detail="Database Host does not exist", status_code=500)
 
 
 DATABASE_PORT = os.getenv("POSTGRES_PORT", None)
 
 if DATABASE_PORT is None:
     print("DATABASE PORT IS UNKNOWN")
-    sys.exit(1)
+    raise HTTPException(detail="Database Port does not exist", status_code=500)
 
 
 DATABASE_NAME = os.getenv("POSTGRES_DATABASE_NAME", None)
 
 if DATABASE_NAME is None:
     print("DATABASE NAME IS UNKNOWN")
-    sys.exit(1)
+    raise HTTPException(detail="Database Name does not exist", status_code=500)
 
 
 DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{int(DATABASE_PORT)}/{DATABASE_NAME}"
