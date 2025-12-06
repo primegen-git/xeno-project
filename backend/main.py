bf6b870740afb96faf99581da7ca5b02c629e5c9
ujjawal kumar <ujjawal17032002@gmail.com>
2025-12-06 16:02:58 +0530

    add auth router

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from database import engine, get_db
 from routers.shops import router as shops_router
 from routers.shopify_sync import router as shopify_sync_router
 from routers.fetch_database import router as fetch_database_router
+from routers.auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -47,3 +48,4 @@ async def check_database(db: Session = Depends(get_db)):
 app.include_router(router=shops_router, prefix="/shops", tags=["shops"])
 app.include_router(router=shopify_sync_router, prefix="/sync", tags=["shops"])
 app.include_router(router=fetch_database_router, prefix="/fetch", tags=["shops"])
+app.include_router(router=auth_router, prefix="/auth", tags=["shops"])
