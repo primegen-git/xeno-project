2ed81a12ba8677ef27b598401d6060fe392f8ddf
ujjawal kumar <ujjawal17032002@gmail.com>
2025-12-07 08:45:55 +0530

    add event router

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends
+from typing import AsyncGenerator
+
+import models
+from database import engine, get_db
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers.auth import router as auth_router
+from routers.events import router as events_router
+from routers.fetch_database import router as fetch_database_router
+from routers.shopify_sync import router as shopify_sync_router
+from routers.shops import router as shops_router
+from routers.webhooks import router as webhooks_router
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-import models
-from typing import AsyncGenerator
-from database import engine, get_db
-from routers.shops import router as shops_router
-from routers.shopify_sync import router as shopify_sync_router
-from routers.fetch_database import router as fetch_database_router
-from routers.auth import router as auth_router
-from routers.webhooks import router as webhooks_router
-from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -55,3 +57,4 @@ app.include_router(router=shopify_sync_router, prefix="/sync", tags=["shops"])
 app.include_router(router=fetch_database_router, prefix="/fetch", tags=["shops"])
 app.include_router(router=auth_router, prefix="/auth", tags=["shops"])
 app.include_router(router=webhooks_router, prefix="/webhooks", tags=["shops"])
+app.include_router(router=events_router, tags=["events"])
